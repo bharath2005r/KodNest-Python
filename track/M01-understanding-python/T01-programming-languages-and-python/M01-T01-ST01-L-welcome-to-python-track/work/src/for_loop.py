f6fe40570ed7a4dd6f3a097bf2b2ec85798ee045
bharath2005r <bharath.jr112005@gmail.com~>
2026-08-05 17:02:28 +0530

    for loop

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/for_loop.py
@@ -0,0 +1,13 @@
+# Read the number and word
+n = int(input())
+word = input()
+
+# Print the number sequence
+print("Numbers:")
+for i in range(1, n + 1):
+    print(i)
+
+# Print the characters
+print("Characters:")
+for j in word:
+    print(j)

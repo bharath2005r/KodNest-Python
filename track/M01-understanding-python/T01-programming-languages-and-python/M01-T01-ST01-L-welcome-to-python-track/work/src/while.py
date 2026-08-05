6e1f03acdbfe517dffa8e8729b753b4aa649c45e
bharath2005r <bharath.jr112005@gmail.com~>
2026-08-05 16:45:17 +0530

    while loop

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/while.py
@@ -0,0 +1,14 @@
+# Read the value of n
+n = int(input())
+
+# Initialize the counter and total
+count = 0
+total = 0
+
+# Calculate the total using a while loop
+while count < n:
+    count += 1
+    total += count
+
+# Display the total
+print("Total:", total)
